e97b08a628e31e76890bfb69f1293ecd63d7fde1
ChaurasiaP <pranshu1431@gmail.com>
2023-01-19 16:07:25 +0530

    added UC3 to check employee is full time or not

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,26 +1,33 @@
 public class EmpWageBuilder {
     public static void main(String[] args) {
 
-        // TO CALCULATE DAILY WAGE OF EMPLOYEE
+        // TO CHECK IF EMPLOYEE IS PRESENT FULL TIME OR PART TIME AND COMPUTE THEIR WAGE FOR THE DAY
 
         // CONSTANT VALUES
-        int IS_PRESENT = 1;
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HR = 20;
-        int EMP_HRS = 8;
 
-        // variable values
-        int random = (int) (Math.random() * 10) % 2;       // TO GET RANDOM OUTPUT AS 0 OR 1
+        //variables
+        int EmpWorkingHrs;
+        int TotalEarnings;
+
+        int random = (int) (Math.random() * 10) % 3;         // TO GET RANDOM OUTPUT AS 0, 1 OR 2
         System.out.println(random);
 
-        int EmpWage;
-
-        // IF random == 1, then employee is PRESENT else ABSENT
-        if (random == IS_PRESENT) {
-            EmpWage = (EMP_RATE_PER_HR * EMP_HRS);
+        // IF EMPLOYEE IS FULL TIME EMP WORKING HRS == 8 , FOR PART TIME EMP HRS == 4
+        if (IS_FULL_TIME == random) {
+            EmpWorkingHrs = 8;
+            TotalEarnings = (EmpWorkingHrs * EMP_RATE_PER_HR);
+            System.out.println("Employee is Full Time with salary: " + TotalEarnings);
+        } else if (IS_PART_TIME == random) {
+            EmpWorkingHrs = 4;
+            TotalEarnings = (EmpWorkingHrs * EMP_RATE_PER_HR);
+            System.out.println("Employee is Part Time with Salary: " + TotalEarnings);
         } else {
-            EmpWage = 0;
+            TotalEarnings = 0;
+            System.out.println("Employee is Absent, hence salary: " + TotalEarnings);
         }
-        System.out.println("Daily Wage of the Employee is: " + EmpWage);
-        System.out.println("UC2 Ends here");
+        System.out.println("UC3 ends");
     }
 }
