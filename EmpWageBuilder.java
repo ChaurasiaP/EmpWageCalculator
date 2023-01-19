ee5e29f3c84e62c98abff2177b19e5d8197b2276
ChaurasiaP <pranshu1431@gmail.com>
2023-01-19 16:11:17 +0530

    modified UC3 with Switch Case

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,33 +1,32 @@
 public class EmpWageBuilder {
     public static void main(String[] args) {
 
-        // TO CHECK IF EMPLOYEE IS PRESENT FULL TIME OR PART TIME AND COMPUTE THEIR WAGE FOR THE DAY
+        // UC3 WITH SWITCH CASE
 
-        // CONSTANT VALUES
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
+        //constants
         int EMP_RATE_PER_HR = 20;
 
         //variables
-        int EmpWorkingHrs;
-        int TotalEarnings;
+        int empWorkingHrs;
+        int dailyWage;
 
         int random = (int) (Math.random() * 10) % 3;         // TO GET RANDOM OUTPUT AS 0, 1 OR 2
         System.out.println(random);
 
-        // IF EMPLOYEE IS FULL TIME EMP WORKING HRS == 8 , FOR PART TIME EMP HRS == 4
-        if (IS_FULL_TIME == random) {
-            EmpWorkingHrs = 8;
-            TotalEarnings = (EmpWorkingHrs * EMP_RATE_PER_HR);
-            System.out.println("Employee is Full Time with salary: " + TotalEarnings);
-        } else if (IS_PART_TIME == random) {
-            EmpWorkingHrs = 4;
-            TotalEarnings = (EmpWorkingHrs * EMP_RATE_PER_HR);
-            System.out.println("Employee is Part Time with Salary: " + TotalEarnings);
-        } else {
-            TotalEarnings = 0;
-            System.out.println("Employee is Absent, hence salary: " + TotalEarnings);
+        switch (random) {
+            case 1:
+                System.out.println("Employee is Present FULL TIME.");
+                empWorkingHrs = 8;
+                break;
+            case 2:
+                System.out.println("Employee is Present Part Time.");
+                empWorkingHrs = 4;
+                break;
+            default:
+                System.out.println("Employee is ABSENT.");
+                empWorkingHrs = 0;
         }
-        System.out.println("UC3 ends");
+        dailyWage = (EMP_RATE_PER_HR * empWorkingHrs);
+        System.out.println("Employee has worked " + empWorkingHrs + ", hence earned a salary of Rs. " + dailyWage);
     }
 }
