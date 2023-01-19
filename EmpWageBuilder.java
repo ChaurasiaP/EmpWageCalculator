f13ba68be39ba17f057c1d27ba5055e46680f3eb
ChaurasiaP <pranshu1431@gmail.com>
2023-01-19 15:49:58 +0530

    added UC1 to check attendence

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,21 @@
 public class EmpWageBuilder {
     public static void main(String[] args) {
-        System.out.println("WELCOME TO EMPLOYEE WAGE PROBLEMS");
+
+        // TO CHECK IF EMPLOYEE IS PRESENT OR ABSENT
+
+        // CONSTANT VARIABLE
+        int IS_PRESENT = 1;
+
+        // variable values
+        int random = (int) (Math.random() * 10) % 2;            // TO GET RANDOM OUTPUT AS 0 OR 1
+        System.out.println(random);
+
+        // IF random == 1, then employee is PRESENT else ABSENT
+
+        if (random == IS_PRESENT) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
